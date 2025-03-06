3350233b83c975763f9d09504eced7afd4e4be2c
iRpro16 <rumeoisidoro16@gmail.com>
2025-03-05 22:45:17 -0500

    UI: Home and NavBar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,17 +1,37 @@
 import ImageCarousel from "./ImageCarousel";
 import { useOutletContext } from "react-router-dom";
+import "../styles/Home.css"
 
 function Home() {
     const {albums} = useOutletContext();
 
     return (
-        <div>
-            <h1>This is the Home page!</h1>
-            {albums === null || albums.length == 0 ? (
-                <p>Loading...</p>
-            ) : (
-                <ImageCarousel albums={albums} />
-            )}
+        <>
+            <div className="home-component">
+                <h1>Check out our hottest sellers!</h1>
+                {albums === null || albums.length == 0 ? (
+                    <p>Loading...</p>
+                ) : (
+                    <ImageCarousel albums={albums}/>
+                )}
+            </div>
+            <HomeDescription />
+        </>
+    )
+}
+
+function HomeDescription() {
+    return (
+        <div className="home-description">
+            <p>
+                Browse our extensive catalog of vinyl records, 
+                from rap to latin, with detailed product 
+                descriptions and high-resolution images. 
+                Our web app features secure online ordering, add to cart, 
+                and personalized recommendations to enhance your shopping experience
+                Our web app is more than just an online store; it is a community for vinyl lovers. 
+                Discover new music, rediscover old favorites, and build your dream collection!
+            </p>
         </div>
     )
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,11 +4,21 @@ import "../styles/NavBar.css";
 function NavBar({ numberItems }) {
     return (
         <div className="nav-bar">
-            <h3>The Vinyl Store</h3>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="shop">Shop</Link></li>
-                <li><Link to="cart">Cart({numberItems})</Link></li>
+            <div className="store-header">
+                <img src="./public/vinyl.png" id="vinyl-svg"/>
+                <h3>The Vinyl Store</h3>
+            </div>
+
+            <ul className="page-links">
+                <li>
+                    <Link to="/" style={{ textDecoration: "none", color: "black" }}>Home</Link>
+                </li>
+                <li>
+                    <Link to="shop" style={{ textDecoration: "none", color: "black"}}>Shop</Link>
+                </li>
+                <li>
+                    <Link to="cart" style={{ textDecoration: "none", color: "black" }}>Cart({numberItems})</Link>
+                </li>
             </ul>
         </div>
     );
